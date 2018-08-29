4790905584595470c86e669fa412dc1b7813ce62
IT FAN <1724940950@qq.com>
2018-08-29 09:37:13 +0800

    实现项目的自动加载

---
+++ controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+namespace controller;
+class UserController{
+    public function hello(){
+        // 取数据
+        $user = new User;
+        $name = $user->getName();
+        // 加载视图
+        view('users.hello',['name'=>$name]);
+        
+        
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+// 定义常量
+define('ROOT',dirname(__FILE__)."/../");
+// 实现自动加载
+function autoload(){
+    $path = str_replace('\\','/',$class);
+    require(ROOT.$path.'.php');
+
+}
+spl_autoload_register('autoload');
+// 添加路由：解析url上的路径：控制器/方法
+// 获取url上的路径
+
+if(isset($_SERVER['PATH_INFO'])){
+    $pathInfo = $_SERVER['PATH_INFO'];
+    $pathInfo = explode('/',$pathInfo);
+
+}else{
+    // 默认控制器和方法
+    $controller = 'IndexController';
+    $action = 'index';
+
+}
+//为控制器添加命名空间
+$fullController = 'controllers\\'.$controller;
+$_C = new $fullController;
+$_C->$action();
+
+// 加载视图
+function view($viewFileName,$data=[]){
+    extract($data);
+    $path = str_replace('.','/',$viewFileName);
+    // 加载视图
+    require(ROOT.'views/'.$path);
+}
